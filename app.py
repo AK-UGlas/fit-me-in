991f9c084e9edf1bb0aee9e710cf74bacf13a8a6
Allen_K <allenkelly278@gmail.com>
2021-04-25 12:23:22 +0100

    add header + footer templates

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    render_template('index.html', title="FitMeIn | Book your next fitness class now")
+    return render_template('index.html', title="FitMeIn | Book your next fitness class now")
 
 if __name__ == '__main__':
     app.run(debug=True)
