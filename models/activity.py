b9dfca5144746799dbde2d4f5679387bf2ffc332
Allen_K <allenkelly278@gmail.com>
2021-04-25 12:27:04 +0100

    add activity class

---
+++ models/activity.py
@@ -0,0 +1,7 @@
+class Activity():
+    def __init__(self, name, start_time, duration, location, id=None):
+        self.name = name
+        self.start = start_time
+        self.duration = duration
+        self.location = location
+        self.id = id
