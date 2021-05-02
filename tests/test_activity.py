9bb916eb1ace3bd257bee698318a4ae2a9447d45
Allen_K <allenkelly278@gmail.com>
2021-05-02 16:12:08 +0100

    update tests for Activity model

--- tests/test_activity.py
+++ tests/test_activity.py
@@ -4,15 +4,27 @@ from models.location import Location
 
 class TestActivity(unittest.TestCase):
     def setUp(self):
-        now = datetime.datetime.now()
+        self.now = datetime.datetime.now()
         deltas = [datetime.timedelta(minutes=60*hour) for hour in range(2, 10, 2)]
         self.tiny_room = Location("wee room", 3)
-        self.yoga = Activity("yoga", start_time=now, location=self.tiny_room)
+        self.yoga = Activity("yoga", start_time=self.now, location=self.tiny_room)
 
 # test attributes
 
+    def test_activity_has_name(self):
+        self.assertEqual("yoga", self.yoga.name)
+
+    def test_activity_has_start_time(self):
+        self.assertEqual(self.now, self.yoga.start)
+
+    def test_activity_has_location(self):
+        self.assertEqual(self.tiny_room, self.yoga.location)
+
     def test_activity_id_is_none(self):
         self.assertEqual(None, self.yoga.id)
-
     
+# test methods
+
+    def test_return_date_as_iso8601_format(self):
+        self.assertEqual(self.now.date().isoformat(), self.yoga.get_date(iso=True))
         
