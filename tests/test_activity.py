0bd8d2e1c6e246f0991732fd0c9b2a4657035585
Allen_K <allenkelly278@gmail.com>
2021-05-02 16:02:09 +0100

    update tests

--- tests/test_activity.py
+++ tests/test_activity.py
@@ -1,9 +1,18 @@
-import unittest
-from datetime import date
+import unittest, datetime
 from models.activity import Activity
 from models.location import Location
 
 class TestActivity(unittest.TestCase):
     def setUp(self):
-        self.yoga = Activity("yoga", )
+        now = datetime.datetime.now()
+        deltas = [datetime.timedelta(minutes=60*hour) for hour in range(2, 10, 2)]
         self.tiny_room = Location("wee room", 3)
+        self.yoga = Activity("yoga", start_time=now, location=self.tiny_room)
+
+# test attributes
+
+    def test_activity_id_is_none(self):
+        self.assertEqual(None, self.yoga.id)
+
+    
+        
